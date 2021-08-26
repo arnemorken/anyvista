d51856e3e3f39d70b5ff0577ed032d62d1219a34
Arne Morken <arne.morken@gmail.com>
2021-08-26 11:20:09 +0200

    Added copyright/license notice.

--- view/anyStrings_nb-NO.js
+++ view/anyStrings_nb-NO.js
@@ -1,3 +1,13 @@
+/*
+****************************************************************************************
+*
+* anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+*
+* @license AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+* Get licences here: http://balanse.info/anylist/license/ (coming soon).
+* 
+****************************************************************************************
+*/
 var i18n = {
     general: {
     },
@@ -10,8 +20,10 @@ var i18n = {
   
     // Error codes
     error: {
+      CALLBACK_MISSING:    "Mangler callback funksjon eller kontekst. ",
       OPTIONS_MISSING:     "Mangler options. ",
       TYPE_MISSING:        "Mangler type. ",
+      ID_MISSING:          "Mangler id. ",
       ID_ILLEGAL:          "Id må være et positivt heltall eller en streng. ",
     },
   };
