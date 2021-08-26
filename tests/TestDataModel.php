22c8ffcd7042f82eaabd01d90a0d78846b72eb3c
Arne Morken <arne.morken@gmail.com>
2021-08-26 11:52:05 +0200

    Added copyright/license notice.

--- tests/TestDataModel.php
+++ tests/TestDataModel.php
@@ -1,3 +1,13 @@
+<!--
+ ****************************************************************************************
+ *
+ * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+ *
+ * @license AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+ * Get licences here: http://balanse.info/anylist/license/ (coming soon).
+ * 
+ ****************************************************************************************
+-->
 <!doctype html>
 <html lang="en">
 <head>
