8790743dfff1fe7902e6da366dbd019a553b4a9d
Arne Morken <arne.morken@gmail.com>
2021-08-26 12:23:54 +0200

    Added copyright/license notice.

--- data/any_defs.php
+++ data/any_defs.php
@@ -1,3 +1,13 @@
+<!--
+ ****************************************************************************************
+ *
+ * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+ *
+ * @license AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+ * Get licences here: http://balanse.info/anylist/license/ (coming soon).
+ * 
+ ****************************************************************************************
+-->
 <?php
 //
 // User-editable "constants" that define some basic properties of the AnyList API.
