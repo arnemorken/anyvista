40e1e1889443204ccb559aab5d7221bbad068290
Arne Morken <arne.morken@gmail.com>
2021-08-26 11:20:30 +0200

    Added copyright/license notice.

--- view/anyDataModel.js
+++ view/anyDataModel.js
@@ -2,13 +2,13 @@
 /**
  ****************************************************************************************
  *
- * anyList is copyright (C) 2012-2021 Arne D. Morken and Balanse Software.
+ * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
  *
  * @license AGPLv3.0 for open source use or anyList Commercial License for commercial use.
  * Get licences here: http://balanse.info/anylist/license/ (coming soon).
  * 
  ****************************************************************************************
-
+ *
  * __anyDataModel: Tree structure data model that can manipulate data as lists and items
  * and optionally synchronize with a database.__
  *
--- view/anyStrings_en-GB.js
+++ view/anyStrings_en-GB.js
@@ -1,3 +1,13 @@
+/*
+****************************************************************************************
+*
+* anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+*
+* @license AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+* Get licences here: http://balanse.info/anylist/license/ (coming soon).
+* 
+****************************************************************************************
+*/
 var i18n = {
     general: {
     },
@@ -10,8 +20,10 @@ var i18n = {
   
     // Error codes
     error: {
+      CALLBACK_MISSING:    "Callback function or context missing. ",
       OPTIONS_MISSING:     "Options missing. ",
       TYPE_MISSING:        "Type missing. ",
+      ID_MISSING:          "Id missing. ",
       ID_ILLEGAL:          "Id must be a positive integer or a string. ",
     },
   };
