7c3a8962ff83ffdc9a9222069c86fc68064a1cd1
Arne Morken <arne.morken@gmail.com>
2021-09-08 22:21:29 +0200

    Abstract classes for PDO database connection and table.

---
+++ data/db/dbConnection.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ ****************************************************************************************
+ *
+ * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+ *
+ * @license AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+ * Get licences here: http://balanse.info/anylist/license/ (coming soon).
+ *
+ ****************************************************************************************
+*/
+require_once "dbDefs.php";
+
+/**
+ * __Abstract base class for a PDO database connection.__
+ *
+ * @class dbConnection
+ * @constructor
+ * @example
+ *      new dbConnection()
+ *
+*/
+class dbConnection
+{
+  var $mDBHandle = null;
+  var $mError    = "";
+
+  // Constructor: Connect to database
+  public function __construct()
+  {
+    $dsn = DB_TYPE.":dbname=".DB_NAME.";host=".DB_HOST.";charset=".DB_CHARSET;
+    try {
+      $this->mDBHandle = new PDO($dsn, DB_USER, DB_PASS);
+    }
+    catch (PDOException $e) {
+      $this->mError = $e->getMessage();
+      error_log($this->mError);
+      echo 'dbConnection: Connection failed: ' . $this->mError;
+    }
+  }
+
+  function isError()
+  {
+    return isset($this->mError) && $this->mError !== "" && $this->mDBHandle->errorCode() !== null;
+  }
+
+  function getError()
+  {
+    if ($this->mDBHandle == null)
+      return "No connection. ";
+    $err = $this->mDBHandle->errorInfo();
+    return $this->mError.". Error ".$err[0].":".$err[2];
+  }
+
+} // class dbConnection
+?>
---
+++ data/db/dbDefs.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ ****************************************************************************************
+ *
+ * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+ *
+ * @license AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+ * Get licences here: http://balanse.info/anylist/license/ (coming soon).
+ *
+ ****************************************************************************************
+*/
+define('DB_HOST','127.0.0.1');      // Database server
+define('DB_USER','root');           // Database user
+define('DB_PASS','');               // Database password
+define('DB_NAME','any_testserver'); // Database name
+
+define('DB_TYPE',   'mysql');       // Database type
+define('DB_CHARSET','UTF8');        // Database charset
+?>
