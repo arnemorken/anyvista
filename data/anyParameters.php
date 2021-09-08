f14251ed63895abc3b55736dd086593605928251
Arne Morken <arne.morken@gmail.com>
2021-09-08 17:32:06 +0200

    Added copyright notice.

--- data/anyParameters.php
+++ data/anyParameters.php
@@ -1,4 +1,14 @@
 <?php
+/**
+  ****************************************************************************************
+ *
+ * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+ *
+ * @license AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+ * Get licences here: http://balanse.info/anylist/license/ (coming soon).
+ *
+ ****************************************************************************************
+*/
 //
 // Extracts key/value pairs from $_GET, $_POST and/or a string and puts them into an array.
 //
