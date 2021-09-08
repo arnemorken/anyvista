f5e8fb32a6e1ad5bfcc2a150ac40141a55ee3860
Arne Morken <arne.morken@gmail.com>
2021-09-08 17:22:44 +0200

    Changed name of data script to anyGetData.php.

--- view/any_defs.js
+++ view/any_defs.js
@@ -6,7 +6,7 @@
  *
  * @license AGPLv3.0 for open source use or anyList Commercial License for commercial use.
  * Get licences here: http://balanse.info/anylist/license/ (coming soon).
- * 
+ *
  ****************************************************************************************
  */
 //
@@ -15,7 +15,7 @@
 let gServer           = "//localhost/";
 let gAnyListFolder    = "Prosjekter/testserver/wp-content/plugins/anylist/";
 let gThirdpartyFolder = "Prosjekter/testserver/javascript/";
-let gDataScript       = "data/GetData.php";  // Relative to gAnyListFolder
+let gDataScript       = "data/anyGetData.php"; // Relative to gAnyListFolder
 
 //
 // Do not edit below unless you really know what you are doing
