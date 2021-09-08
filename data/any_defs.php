2a1fcbafd17f3360604477b1a78321a6135916ee
Arne Morken <arne.morken@gmail.com>
2021-09-08 17:00:34 +0200

    Fixed comment bug. Added includes for FontAwesome.

--- data/any_defs.php
+++ data/any_defs.php
@@ -1,21 +1,21 @@
-<!--
- ****************************************************************************************
+<?php
+/**
+  ****************************************************************************************
  *
  * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
  *
  * @license AGPLv3.0 for open source use or anyList Commercial License for commercial use.
  * Get licences here: http://balanse.info/anylist/license/ (coming soon).
- * 
+ *
  ****************************************************************************************
--->
-<?php
+*/
 //
 // User-editable "constants" that define some basic properties of the AnyList API.
 //
 define("gServer",           "//localhost/");
 define("gAnyListFolder",    "Prosjekter/testserver/wp-content/plugins/anylist/");
 define("gThirdpartyFolder", "Prosjekter/testserver/javascript/");
-define("gDataScript",       "data/GetData.php");  // Relative to gAnyListFolder
+define("gDataScript",       "data/anyGetData.php"); // Relative to gAnyListFolder
 
 //
 // Do not edit below unless you really know what you are doing.
@@ -34,4 +34,7 @@ define("gDataSource", $_SERVER['DOCUMENT_ROOT'] . "/" . gAnyListFolder . "/" . g
 // jQuery / jQueryUI
 define("gjQueryURL",        gServer            . gThirdpartyFolder . "jquery/");
 define("gjQuery_js",        gjQueryURL         . "jquery-3.6.0.min.js");
+// Font Awesome
+define("gFontAwesomeURL",   gServer            . gThirdpartyFolder . "fontawesome/");
+define("gFontAwesome_css",  gFontAwesomeURL    . "fa.min.css");
 ?>
