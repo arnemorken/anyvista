5ed519059b0b7b4dc52a5f63bceaf86511bff9c8
Arne Morken <arne.morken@gmail.com>
2021-11-10 12:25:16 +0100

    Changed the way plugins are accessed directly (got rid of *Basic.php files).

--- data/plugins/group/group.php
+++ data/plugins/group/group.php
@@ -1,7 +1,47 @@
 <?php
+/****************************************************************************************
+ *
+ * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+ *
+ * License: AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+ * Get licences here: http://balanse.info/anylist/license/ (coming soon).
+ *
+ ****************************************************************************************/
+/*
+ * Include this file in plugins that are using the anyList group plugin with a server backend
+ */
   require_once dirname(__FILE__)."/../../anyDefs.php";
   require_once dirname(__FILE__)."/../client.php";
+  require_once dirname(__FILE__)."/../document/client.php";
   require_once dirname(__FILE__)."/../group/client.php";
   require_once dirname(__FILE__)."/../user/client.php";
-  require_once dirname(__FILE__)."/groupBasic.php";
+  require_once gDataSource;
+  $gViewArea = "any_content";
+  Parameters::set("type","group");
+  $the_data = anyGetData();
 ?>
+<div id="<?php print $gViewArea;?>"/>
+
+<script>
+var serverdata = <?php echo $the_data;?>;
+if (serverdata && serverdata.JSON_CODE)
+  serverdata = serverdata.JSON_CODE;
+var model = new groupDataModel({ data:       serverdata ? serverdata.data : null,
+                                 permission: serverdata ? serverdata.permission : null,
+                                 plugins:    serverdata ? serverdata.plugins : null,
+                                 mode:       "remote",
+                              });
+var data_id      = "<?php echo Parameters::get("group_id");?>";
+var is_admin     = model.permission && model.permission.is_admin;
+var is_logged_in = model.permission && model.permission.is_logged_in && parseInt(model.permission.current_user_id) > 0;
+var is_new       = (data_id == "new" || parseInt(data_id) == -1);
+var is_me        = model.permission && parseInt(model.permission.current_user_id) == parseInt(data_id);
+var view = new groupDataViewTabs({ id:          "<?php print $gViewArea;?>",
+                                   model:       model,
+                                   isEditable:  true, //is_admin || is_new,
+                                   isDeletable: true, //is_admin || is_new,
+                                   isRemovable: false,
+                                   edit:        is_new,
+                                });
+view.refresh(null,null,null,"group");
+</script>
--- data/plugins/user/user.php
+++ data/plugins/user/user.php
@@ -1,7 +1,48 @@
 <?php
+/****************************************************************************************
+ *
+ * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+ *
+ * License: AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+ * Get licences here: http://balanse.info/anylist/license/ (coming soon).
+ *
+ ****************************************************************************************/
+/*
+ * Include this file in plugins that are using the anyList user plugin with a server backend
+ */
   require_once dirname(__FILE__)."/../../anyDefs.php";
   require_once dirname(__FILE__)."/../client.php";
+  require_once dirname(__FILE__)."/../document/client.php";
   require_once dirname(__FILE__)."/../group/client.php";
   require_once dirname(__FILE__)."/../user/client.php";
-  require_once dirname(__FILE__)."/userBasic.php";
+  require_once gDataSource;
+  $gViewArea = "any_content";
+  Parameters::set("type","user");
+  $the_data = anyGetData();
 ?>
+<div id="<?php print $gViewArea;?>"/>
+
+<script>
+var serverdata = <?php echo $the_data;?>;
+if (serverdata && serverdata.JSON_CODE)
+  serverdata = serverdata.JSON_CODE;
+var model = new userDataModel({ data:       serverdata ? serverdata.data : null,
+                                permission: serverdata ? serverdata.permission : null,
+                                plugins:    serverdata ? serverdata.plugins : null,
+                                mode:       "remote",
+                             });
+var data_id      = "<?php echo Parameters::get("user_id");?>";
+var is_admin     = model.permission && model.permission.is_admin;
+var is_logged_in = model.permission && model.permission.is_logged_in && parseInt(model.permission.current_user_id) > 0;
+var is_new       = (data_id == "new" || parseInt(data_id) == -1) && (!is_logged_in || is_admin);
+var is_me        = model.permission && parseInt(model.permission.current_user_id) == parseInt(data_id);
+var view = new userDataViewTabs({ id:          "<?php print $gViewArea;?>",
+                                  model:       model,
+                                  isEditable:  true, //is_admin || is_new || is_me,
+                                  isDeletable: true, //is_admin || is_new || is_me,
+                                  isRemovable: false,
+                                  edit:        is_new,
+                                  dispEmail:   is_admin || is_me || is_new,
+                               });
+view.refresh(null,null,null,"user");
+</script>
