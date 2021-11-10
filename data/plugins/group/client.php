638100436d5a0e25c97d31a506753dc883d4654e
Arne Morken <arne.morken@gmail.com>
2021-11-10 12:34:58 +0100

    Moved plugin css files to subfolder.

--- data/plugins/group/client.php
+++ data/plugins/group/client.php
@@ -1,4 +1,17 @@
-
+<?php
+/****************************************************************************************
+ *
+ * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+ *
+ * License: AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+ * Get licences here: http://balanse.info/anylist/license/ (coming soon).
+ *
+ ****************************************************************************************/
+/*
+ * Include this file in plugins that are using the anyList group plugin with a server backend
+ */
+?>
+<link  href="<?php print gAnyListURL;?>view/plugins/group/skin/<?php print gSkin;?>/group.css" rel="stylesheet"/>
 <script src="<?php print gAnyListURL;?>view/plugins/group/groupDataModel.js"></script>
 <script src="<?php print gAnyListURL;?>view/plugins/group/groupFilter.js"></script>
 <script src="<?php print gAnyListURL;?>view/plugins/group/groupDataViewTabs.js"></script>
--- data/plugins/user/client.php
+++ data/plugins/user/client.php
@@ -1,4 +1,17 @@
-
+<?php
+/****************************************************************************************
+ *
+ * anyList is copyright (C) 2011-2021 Arne D. Morken and Balanse Software.
+ *
+ * License: AGPLv3.0 for open source use or anyList Commercial License for commercial use.
+ * Get licences here: http://balanse.info/anylist/license/ (coming soon).
+ *
+ ****************************************************************************************/
+/*
+ * Include this file in plugins that are using the anyList user plugin with a server backend
+ */
+?>
+<link  href="<?php print gAnyListURL;?>view/plugins/user/skin/<?php print gSkin;?>/user.css" rel="stylesheet"/>
 <script src="<?php print gAnyListURL;?>view/plugins/user/userDataModel.js"></script>
 <script src="<?php print gAnyListURL;?>view/plugins/user/userFilter.js"></script>
 <script src="<?php print gAnyListURL;?>view/plugins/user/userDataViewTabs.js"></script>
