bd4b81a604985fa7b12f65470c625d94fb2457ea
Arne Morken <arne.morken@gmail.com>
2021-11-17 14:17:11 +0100

    Changed name of a few temporary global variables.

--- data/anyDefs.php
+++ data/anyDefs.php
@@ -10,10 +10,10 @@
 //
 // User-editable "constants" that define some basic properties of the anyList API.
 //
-define("gServer",           "//localhost/");
-define("gHomeFolder",       "Prosjekter/testserver/wp-content/plugins/anylist/");
-define("gThirdpartyFolder", "Prosjekter/testserver/javascript/");
-define("gSkin",             "default");
+define("gServer",       "//localhost/");
+define("gHomeFolder",   "Prosjekter/testserver/wp-content/plugins/anylist/");
+define("gThirdpartyJS", "Prosjekter/testserver/javascript/");
+define("gSkin",         "default");
 
 //
 // Do not edit below unless you really know what you are doing.
@@ -40,17 +40,17 @@ define("gUploadPath",   $_SERVER['DOCUMENT_ROOT'] . "/" . gHomeFolder . "/" . gU
 //
 
 // jQuery / jQueryUI
-define("gjQueryURL",        gServer            . gThirdpartyFolder . "jquery/");
+define("gjQueryURL",        gServer            . gThirdpartyJS . "jquery/");
 define("gjQuery_js",        gjQueryURL         . "jquery-3.6.0.min.js");
-define("gjQueryWidgetURL",  gServer            . gThirdpartyFolder . "jquery/");
+define("gjQueryWidgetURL",  gServer            . gThirdpartyJS . "jquery/");
 define("gjQueryWidget_js",  gjQueryWidgetURL   . "jquery-widget-1.12.1.min.js");
 
 // W3CSS
-define("gW3CSSURL",         gServer            . gThirdpartyFolder . "w3css/");
+define("gW3CSSURL",         gServer            . gThirdpartyJS . "w3css/");
 define("gW3CSS_css",        gW3CSSURL          . "w3.css");
 
 // Font Awesome
-define("gFontAwesomeURL",   gServer            . gThirdpartyFolder . "fontawesome/");
+define("gFontAwesomeURL",   gServer            . gThirdpartyJS . "fontawesome/");
 define("gFontAwesome_css",  gFontAwesomeURL    . "fa.min.css");
 
 // Wordpress stuff
