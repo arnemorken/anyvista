3d96893532f843b9d94fd8755132b401ca548376
Arne Morken <arne.morken@gmail.com>
2021-11-18 00:49:51 +0100

    Added regex filter for numeric values.

--- view/functions.js
+++ view/functions.js
@@ -16,6 +16,24 @@ Object.size = function (obj)
   return objsize;
 };
 
+// Filter input by regex, can be used to allow only numeric input values
+(function($) {
+  $.fn.inputFilter = function(inputFilter) {
+    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  };
+}($));
+
 var isInt = function (obj)
 {
   return !isNaN(obj) && parseInt(Number(obj)) == obj && !isNaN(parseInt(obj,10));
