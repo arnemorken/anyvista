893d6570ec2bb79424849427074e307be1087bb7
Arne Morken <arne.morken@gmail.com>
2023-01-24 13:51:41 +0100

    Updated copyright date.

--- data/plugins/group/group.php
+++ data/plugins/group/group.php
@@ -1,7 +1,7 @@
 <?php
 /****************************************************************************************
  *
- * anyVista is copyright (C) 2011-2022 Arne D. Morken and Balanse Software.
+ * anyVista is copyright (C) 2011-2023 Arne D. Morken and Balanse Software.
  *
  * License: AGPLv3.0 for open source use or anyVista Commercial License for commercial use.
  * Get licences here: http://balanse.info/anyvista/license/ (coming soon).
@@ -47,6 +47,7 @@ var view = new groupViewTabs({ id:          "<?php print $gViewArea;?>",
                                isDeletable: true, //is_admin || is_new,
                                isRemovable: false,
                                edit:        is_new,
+                               defaultKind: "list",
                             });
 view.refresh();
 </script>
--- view/plugins/document/documentViewTabs.js
+++ view/plugins/document/documentViewTabs.js
@@ -1,7 +1,7 @@
 "use strict";
 /****************************************************************************************
  *
- * anyVista is copyright (C) 2011-2022 Arne D. Morken and Balanse Software.
+ * anyVista is copyright (C) 2011-2023 Arne D. Morken and Balanse Software.
  *
  * License: AGPLv3.0 for open source use or anyVista Commercial License for commercial use.
  * Get licences here: http://balanse.info/anyvista/license/ (coming soon).
