09e144ab6af77e1afd64ab2d9fbe2096474c51bc
Arne Morken <arne.morken@gmail.com>
2023-07-05 13:40:27 +0200

    Make sure createClass() dont return before the class is created.

--- data/alasql/anyTableFactory.js
+++ data/alasql/anyTableFactory.js
@@ -12,32 +12,50 @@ var anyTableFactory = function (connection)
 // Does not create the actual database table.
 // TODO! What if class already exists (script is loaded)?
 //
-anyTableFactory.prototype.createClass = function(className,parameters)
+anyTableFactory.prototype.createClass = async function(className,parameters,callback)
 {
   if (!className) {
-    this.error = "Class name missing. ";
+    this.error = "Class name missing. "; // TODO! i18n
     console.error(this.error);
     return null;
   }
-  var head = document.getElementsByTagName('head')[0];
-  var js   = document.createElement("script");
-  var path = "types/" + className + ".js";
-  if (!isScriptLoaded(path)) {
-    //console.log("loading "+path);
-    js.src = path;
-  }
+  let res = await this._createClass(className,parameters,callback);
+  return res;
+}; // createClass
+
+anyTableFactory.prototype._createClass = function(className,parameters,callback)
+{
   try {
+  var head = document.getElementsByTagName("head")[0];
+  var js   = document.createElement("script");
+  var path = gServer+gHomeFolder+"data/alasql/types/" + className + ".js";
+  //console.log("loading "+path);
+  let self = this;
+  js.async = false;
+  js.src   = path;
+  let res = new Promise( function(resolve) {
+    js.addEventListener("load", function() {
+      console.log("creating class "+className);
+      let table_class = new window[className](self.connection,parameters);
+      table_class.className = className;
+      //if (callback)
+      //  callback(table_class);
+      return resolve(table_class);
+    });
+    js.onerror = function(e){
+      let errstr = "Could not find "+path;
+      //console.error(errstr);
+      self.error = errstr;
+      return resolve({error: errstr});
+    };
     head.appendChild(js);
-    let table_class = new window[className](this.connection,parameters);
-    table_class.className = className;
-    return table_class;
+  });
+  return res;
   }
   catch (err) {
-    this.error = "Could not initialise "+className+":"+err;
-    console.error(this.error);
-    return null;
+    console.log(err);
   }
-}; // createClass
+}; // _createClass
 
 function isScriptLoaded(src)
 {
