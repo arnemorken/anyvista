d58e3f06bc2f263ce63dcc98fee64511a0b39eb6
Arne Morken <arne.morken@gmail.com>
2023-12-10 12:25:28 +0100

    Bugfix on dbUpdate()

--- view/anyView.js
+++ view/anyView.js
@@ -4511,7 +4511,7 @@ $.any.anyView.prototype.dbUpdate = function (event)
       let res = icdiv.find($(".fas"));
       res.toggleClass('fa-pencil-alt').toggleClass('fa-solid fa-spinner fa-spin'); // TODO! CSS
     }
-    if (par_id && par_data[par_id] && (par_data[par_id].head == "group" || par_data[par_id].item == "group" || par_data[par_id].list == "group"))
+    if (par_id && par_data && par_data[par_id] && (par_data[par_id].head == "group" || par_data[par_id].item == "group" || par_data[par_id].list == "group"))
       event.data.group_id = par_id;
     return this.model.dbUpdate(event.data);
   }
