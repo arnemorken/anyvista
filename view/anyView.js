63f4875c515deda4400f3297f838921d4c7d460f
Arne Morken <arne.morken@gmail.com>
2024-03-10 19:48:34 +0100

    Update to newest tinymce version.

--- view/anyView.js
+++ view/anyView.js
@@ -5095,25 +5095,63 @@ $.any.anyView.prototype.initComponents = function ()
   // TinyMCE
   if (typeof tinyMCE != "undefined") {
     tinymce.init({
-      width:      "100%",
-      mode:       "specific_textareas",
       selector:   ".tinymce",
-      theme:      "modern",
+      width:      "100%",
+      resize:     true,
       menubar:    false,
       statusbar:  false,
       force_br_newlines: false,
-      force_p_newlines:  false,
-      forced_root_block: false, // Or ''?
-      plugins: [
-        "link image media autoresize lists"
-      ],
-      autoresize_on_init: true,
-      autoresize_min_height: 0,
-      autoresize_max_height: 200,
+      forced_root_block: 'p',
+      plugins: ["link", "image", "media", "autoresize", "lists"],
       autoresize_bottom_margin: 0,
       toolbar_items_size: "small",
+      theme:      "silver",
+      skin: "tinymce-5",
+      content_style: "body { margin: .5em; line-height: 1; }",
       toolbar1: "undo redo | cut copy paste | link unlink anchor image media code | bullist numlist | outdent indent blockquote | alignleft aligncenter alignright alignjustify | hr",
       toolbar2: "bold italic underline strikethrough subscript superscript | styleselect formatselect fontselect fontsizeselect | searchreplace",
+
+  /* enable title field in the Image dialog*/
+  image_title: true,
+  /* enable automatic uploads of images represented by blob or data URIs*/
+  automatic_uploads: true,
+  /*
+    URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
+    images_upload_url: 'postAcceptor.php',
+    here we add custom filepicker only to Image dialog
+  */
+  file_picker_types: 'image',
+  /* and here's our custom image picker*/
+  file_picker_callback: (cb, value, meta) => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+
+    input.addEventListener('change', (e) => {
+      const file = e.target.files[0];
+
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        /*
+          Note: Now we need to register the blob in TinyMCEs image blob
+          registry. In the next release this part hopefully won't be
+          necessary, as we are looking to handle it internally.
+        */
+        const id = 'blobid' + (new Date()).getTime();
+        const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+        const base64 = reader.result.split(',')[1];
+        const blobInfo = blobCache.create(id, file, base64);
+        blobCache.add(blobInfo);
+
+        /* call the callback and populate the Title field with the file name */
+        cb(blobInfo.blobUri(), { title: file.name });
+      });
+      reader.readAsDataURL(file);
+    });
+
+    input.click();
+  },
+
     });
   }
   return this;
