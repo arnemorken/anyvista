07da82fbc261d906acf6d201ab6ea6b4660f236a
Arne Morken <arne.morken@gmail.com>
2024-07-12 12:06:36 +0200

    Cosmetic changes.

--- view/anyView.js
+++ view/anyView.js
@@ -5166,48 +5166,46 @@ $.any.anyView.prototype.initComponents = function ()
       skin: "tinymce-5",
       content_style: "body { margin: .5em; line-height: 1; }",
       toolbar1: "bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent blockquote | table hr link unlink anchor image media code | undo redo | cut copy paste",
+      license_key: "gpl",
+      /* enable title field in the Image dialog*/
+      image_title: true,
+      /* enable automatic uploads of images represented by blob or data URIs*/
+      automatic_uploads: true,
+      /*
+        URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
+        images_upload_url: 'postAcceptor.php',
+        here we add custom filepicker only to Image dialog
+      */
+      file_picker_types: 'image',
+      /* and here's our custom image picker*/
+      file_picker_callback: (cb, value, meta) => {
+        const input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
 
-  /* enable title field in the Image dialog*/
-  image_title: true,
-  /* enable automatic uploads of images represented by blob or data URIs*/
-  automatic_uploads: true,
-  /*
-    URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
-    images_upload_url: 'postAcceptor.php',
-    here we add custom filepicker only to Image dialog
-  */
-  file_picker_types: 'image',
-  /* and here's our custom image picker*/
-  file_picker_callback: (cb, value, meta) => {
-    const input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/*');
+        input.addEventListener('change', (e) => {
+          const file = e.target.files[0];
 
-    input.addEventListener('change', (e) => {
-      const file = e.target.files[0];
+          const reader = new FileReader();
+          reader.addEventListener('load', () => {
+            /*
+              Note: Now we need to register the blob in TinyMCEs image blob
+              registry. In the next release this part hopefully won't be
+              necessary, as we are looking to handle it internally.
+            */
+            const id = 'blobid' + (new Date()).getTime();
+            const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+            const base64 = reader.result.split(',')[1];
+            const blobInfo = blobCache.create(id, file, base64);
+            blobCache.add(blobInfo);
 
-      const reader = new FileReader();
-      reader.addEventListener('load', () => {
-        /*
-          Note: Now we need to register the blob in TinyMCEs image blob
-          registry. In the next release this part hopefully won't be
-          necessary, as we are looking to handle it internally.
-        */
-        const id = 'blobid' + (new Date()).getTime();
-        const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-        const base64 = reader.result.split(',')[1];
-        const blobInfo = blobCache.create(id, file, base64);
-        blobCache.add(blobInfo);
-
-        /* call the callback and populate the Title field with the file name */
-        cb(blobInfo.blobUri(), { title: file.name });
-      });
-      reader.readAsDataURL(file);
-    });
-
-    input.click();
-  },
-
+            /* call the callback and populate the Title field with the file name */
+            cb(blobInfo.blobUri(), { title: file.name });
+          });
+          reader.readAsDataURL(file);
+        });
+        input.click();
+      },
     });
   }
   return this;
